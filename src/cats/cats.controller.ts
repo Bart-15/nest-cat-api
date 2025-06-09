f9f5753c7081876275b6fb24e82e988b4d4c3eba
Bart-15 <brat.tabusao@gmail.com>
2025-06-09 21:14:34 +0800

    feat: add crud functionality

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -10,40 +10,37 @@ import {
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
   @Get()
-  getCats(@Query('type') type: string) {
-    return [{ type }];
+  getCats(@Query('color') color: string) {
+    return this.catsService.getCats(color);
   }
 
   @Get(':id')
   getCat(@Param('id') id: string) {
-    return {
-      catId: id,
-    };
+    return this.catsService.getCatById(id);
   }
 
   @Post()
   createCat(@Body() createCatDto: CreateCatDto) {
-    return {
-      name: createCatDto.name,
-    };
+    const newCat = createCatDto;
+    this.catsService.createCat(createCatDto);
+
+    return newCat;
   }
 
   @Put(':id')
   updateCat(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return {
-      catId: id,
-      name: updateCatDto,
-    };
+    return this.catsService.updateCat(id, updateCatDto);
   }
 
   @Delete(':id')
   deleteCat(@Param('id') id: string) {
-    return {
-      catId: id,
-    };
+    return this.catsService.deleteCat(id);
   }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,68 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  private cats = [
+    {
+      id: uuidv4(),
+      name: 'Max',
+      color: 'black',
+    },
+    {
+      id: uuidv4(),
+      name: 'Pusong',
+      color: 'white',
+    },
+  ];
+
+  getCats(color?: string) {
+    if (color) {
+      return this.cats.filter((cat) => cat.color === color);
+    }
+
+    return this.cats;
+  }
+
+  getCatById(id: string) {
+    const cat = this.cats.find((cat) => cat.id === id);
+
+    if (!cat) {
+      throw new Error('Cat not found');
+    }
+
+    return cat;
+  }
+
+  createCat(createCatDto: CreateCatDto) {
+    const newCat = {
+      id: uuidv4(),
+      ...createCatDto,
+    };
+
+    this.cats.push(newCat);
+  }
+
+  updateCat(id: string, updateCatDto: UpdateCatDto) {
+    this.cats = this.cats.map((cat) => {
+      if (cat.id === id) {
+        return { ...cat, ...updateCatDto };
+      }
+
+      return cat;
+    });
+
+    return this.getCatById(id);
+  }
+
+  deleteCat(id: string) {
+    const toBeRemoved = this.getCatById(id);
+
+    this.cats = this.cats.filter((cat) => cat.id !== id);
+
+    return toBeRemoved;
+  }
+}
